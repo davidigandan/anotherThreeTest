cdb217f463349d51fe7a4826474e338c2534ea92
David Igandan <davidigandan@gmail.com>
2024-10-22 14:06:14 +0100

    latest stable

--- main.js
+++ main.js
@@ -21,14 +21,13 @@ const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cylinder = new THREE.Mesh( geometry, material );
 cylinder.position.set(0,0,0);
 
-// Create top-end = topEndCylinder - topEndCube {rotated}
-
+// Create topEndMesh = topEndCylinder - topEndCube{rotated}
 // Create topEndCylinder
 const topEndCylinderGeometry= new THREE.CylinderGeometry( 2.5, 2.5, 5, 32 );
 const topEndCylinderMaterial = new THREE.MeshBasicMaterial( { color: 0x0000FF } );
 const topEndCylinder = new THREE.Mesh( topEndCylinderGeometry, topEndCylinderMaterial );
 topEndCylinder.position.set(0,12.5,0); //topendcylinder height is 5, so position must be half hight plus height of body
-
+scene.add(topEndCylinder);
 
 // Create topEndCube
 const topEndCubeGeometry = new THREE.BoxGeometry(Math.sqrt(50), Math.sqrt(50), Math.sqrt(50));
@@ -37,22 +36,20 @@ const topEndCube = new THREE.Mesh( topEndCubeGeometry, topEndCubeMaterial);
 topEndCube.position.set(2.5,15,0); //position on top right of cylinder
 topEndCube.rotation.z = (2*Math.PI)-(Math.PI/4); //rotate cube
 topEndCube.updateMatrix(); //store the rotation
-// topEndCube.geometry.applyMatrix4(topEndCube.matrix); 
-// topEndCube.updateMatrixWorld(true); 
+scene.add(topEndCube);
 
 // Perform subtraction
-
 const topEndCylinderCSG = CSG.fromMesh(topEndCylinder, 0);
 const topEndCubeCSG = CSG.fromMesh(topEndCube, 1);
-const topEndCSG = topEndCylinderCSG.subtract(topEndCubeCSG);
-const topEndMesh = CSG.toMesh(topEndCSG, new THREE.Matrix4(), [topEndCylinderMaterial, topEndCubeMaterial]);
+const topEndMeshCSG = topEndCylinderCSG.subtract(topEndCubeCSG);
+const topEndMesh = CSG.toMesh(topEndMeshCSG, new THREE.Matrix4(), topEndCylinderMaterial);
+scene.add(topEndMesh);
 
 topEndMesh.position.set(10,5,0);
-console.log("My object is", topEndMesh);
 
-const axesHelper = new THREE.AxesHelper( 5 ); 
+const axesHelper = new THREE.AxesHelper( 50 ); 
 
-scene.add( axesHelper, cylinder, topEndCylinder, topEndCube, topEndMesh);
+scene.add( axesHelper, cylinder );
 
 
 function animate() {
